65cbbdef1d4d4234d5c9d523c8f61202e8c4d393
ThreatToSurvival <44134496+ThreatToSurvival@users.noreply.github.com>
2018-11-12 00:23:41 +0300

    ДЗ-3 (3)

---
+++ ДЗ-3 (11.11.2018)/Задание 10.cpp
@@ -0,0 +1,30 @@
+/*Напишите процедуру, удаляющую из динамически выделенного массива все четные значения (функция принимает в качестве аргументов – указатель на массив и его размер).
+По окончании работы процедуры количество выделенной памяти должно уменьшиться.*/
+
+#include <iostream>
+
+using namespace std;
+
+bool even(int q)
+{
+	if (q % 2 == 0)
+		return true;
+	else
+		return false;
+}
+
+void shinedown(int * G, int n)
+{
+	for (int i = 1; i <= n; i++)
+	{
+		if (even())
+	}
+}
+
+int main()
+{
+	int b;
+	cin >> b;
+	F = new int [b];
+	shinedown(F[b])
+}
