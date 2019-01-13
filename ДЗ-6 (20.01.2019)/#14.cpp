dc36a6a5e0022c8109532330abd1b34d40dbd3f4
ThreatToSurvival <44134496+ThreatToSurvival@users.noreply.github.com>
2019-01-13 16:24:48 +0300

    🌚🌚🌚🌚🌚

---
+++ ДЗ-6 (20.01.2019)/#14.cpp
@@ -0,0 +1,36 @@
+/* Напишите программу, находящую для заданной последовательности длины N
+неотрицательных целых чисел максимальную сумму двух её
+элементов, номера которых различаются не менее чем на 5. Программа должна
+быть эффективной по времени и памяти */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int N, x;
+	cin >> N;
+	int F[N];
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> F[i];
+		if (F[i] < 0)
+		{
+			break;
+			cout << "The array must include only non-negative numbers";
+		}
+	}
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = 7; j <= N; j++)
+		{
+			if ((F[i] + F[j - 1]) > (F[i] + F[j]))
+				x = F[i] + F[j - 1];
+			else
+				x = F[i] + F[j];
+		}
+	}
+	cout << x;
+	return 0;
+}
